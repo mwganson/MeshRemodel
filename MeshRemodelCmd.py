705357f122708affdacfbb4ca4028ffe6fbdd506
mwganson <mwganson@gmail.com>
2021-10-01 17:15:30 -0500

    v1.89.1 -- test update notifier

--- MeshRemodelCmd.py
+++ MeshRemodelCmd.py
@@ -27,8 +27,8 @@ __title__   = "MeshRemodel"
 __author__  = "Mark Ganson <TheMarkster>"
 __url__     = "https://github.com/mwganson/MeshRemodel"
 __date__    = "2021.10.01"
-__version__ = "1.89"
-version = 1.89
+__version__ = "1.89.1"
+version = 1.89.1
 
 import FreeCAD, FreeCADGui, Part, os, math
 from PySide import QtCore, QtGui
