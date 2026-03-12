f017ad4e4bf04818a46855fd16d2a2b10d831841
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2026-03-12 12:42:52 -0400

    Use 2.1 copyright header

--- InitGui.py
+++ InitGui.py
@@ -2,27 +2,22 @@
 # SPDX-FileCopyrightText: 2019 Mark Ganson <mwganson@gmail.com>
 # SPDX-FileNotice: Part of the Mesh Remodel addon.
 
-###################################################################################
-#
-#  InitGui.py
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
-###################################################################################
+################################################################################
+#                                                                              #
+#   This addon is free software: you can redistribute it and/or modify it      #
+#   under the terms of the GNU Lesser General Public License as published      #
+#   by the Free Software Foundation, either version 2.1 of the License,        #
+#   or (at your option) any later version.                                     #
+#                                                                              #
+#   This addon is distributed in the hope that it will be useful,              #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty                #
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    #
+#   See the GNU Lesser General Public License for more details.                #
+#                                                                              #
+#   You should have received a copy of the GNU Lesser General Public           #
+#   License along with this addon. If not, see https://www.gnu.org/licenses    #
+#                                                                              #
+################################################################################
 
 import meshremodelwb_locator
 meshremodelWBPath = os.path.dirname(meshremodelwb_locator.__file__)
